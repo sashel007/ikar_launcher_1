13e96d3feceb1f269a648dfbf2672aecb0834926
Alexander Sidorkin <sashel007@yandex.ru>
2023-07-05 08:42:22 +0300

    shows the grid after touching the button

--- app/src/main/java/ru/ikar/ikar_launcher/MainActivity.kt
+++ app/src/main/java/ru/ikar/ikar_launcher/MainActivity.kt
@@ -8,26 +8,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var packageManager: PackageManager
+    private var showAllApps by mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +39,9 @@ class MainActivity : ComponentActivity() {
         val userInstalledApps = filterSystemApps(installedApps)
 
         setContent {
-            AppLauncher(installedApps)
+            AppLauncher(installedApps,showAllApps) {
+                showAllApps = !showAllApps
+            }
         }
     }
 
@@ -49,40 +53,41 @@ class MainActivity : ComponentActivity() {
         return packageManager.queryIntentActivities(intent, 0)
     }
 
+    //фильтр, выдающий только системные аппки
     private fun filterSystemApps(apps: List<ResolveInfo>): List<ResolveInfo> {
         return apps.filter { app ->
             app.activityInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0
         }
     }
-
-//    private fun getInstalledApps(packageManager: PackageManager): List<ApplicationInfo> {
-//        val intent = Intent(Intent.ACTION_MAIN, null).apply {
-//            addCategory(Intent.CATEGORY_LAUNCHER)
-//        }
-//        val resolveInfoList = packageManager.queryIntentActivities(intent, 0)
-//        val installedApps = mutableListOf<ApplicationInfo>()
-//
-//        for (resolveInfo in resolveInfoList) {
-//            val packageName = resolveInfo.activityInfo.packageName
-//            val packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA)
-//            val appInfo = packageInfo.applicationInfo
-//            installedApps.add(appInfo)
-//        }
-//
-//        return installedApps
-//    }
 }
 
 @Composable
-fun AppLauncher(installedApps: List<ResolveInfo>) {
+fun AppLauncher(
+    installedApps: List<ResolveInfo>,
+    showAllApps: Boolean,
+    onToggleAllApps: () -> Unit
+) {
     val context = LocalContext.current
     val columns = 4
     val cells = GridCells.Fixed(columns)
 
-    LazyVerticalGrid(cells) {
-        items(installedApps.size) { index ->
-            val app = installedApps[index]
-            AppItem(app, context.packageManager)
+    Box(Modifier.fillMaxSize()) {
+        if (showAllApps) {
+            LazyVerticalGrid(cells) {
+                items(installedApps.size) { index ->
+                    val app = installedApps[index]
+                    AppItem(app, context.packageManager)
+                }
+            }
+        }
+
+        Button(
+            onClick = { onToggleAllApps() },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Text(text = if (showAllApps) "Hide All Apps" else "Show All Apps")
         }
     }
 }
@@ -122,3 +127,20 @@ fun AppItem(app: ResolveInfo, packageManager: PackageManager) {
 //        modifier = Modifier.size(48.dp)
 //    )
 //}
+
+//    private fun getInstalledApps(packageManager: PackageManager): List<ApplicationInfo> {
+//        val intent = Intent(Intent.ACTION_MAIN, null).apply {
+//            addCategory(Intent.CATEGORY_LAUNCHER)
+//        }
+//        val resolveInfoList = packageManager.queryIntentActivities(intent, 0)
+//        val installedApps = mutableListOf<ApplicationInfo>()
+//
+//        for (resolveInfo in resolveInfoList) {
+//            val packageName = resolveInfo.activityInfo.packageName
+//            val packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_META_DATA)
+//            val appInfo = packageInfo.applicationInfo
+//            installedApps.add(appInfo)
+//        }
+//
+//        return installedApps
+//    }
