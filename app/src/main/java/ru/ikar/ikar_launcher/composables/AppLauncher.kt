a604cf8f0d9b9c63368a5607b66eaa9aa30ad19a
Alexander Sidorkin <sashel007@yandex.ru>
2023-08-31 12:41:45 +0300

    при нажатии на область page затемнение не возникает

--- app/src/main/java/ru/ikar/ikar_launcher/composables/AppLauncher.kt
+++ app/src/main/java/ru/ikar/ikar_launcher/composables/AppLauncher.kt
@@ -4,8 +4,6 @@ import android.content.pm.ResolveInfo
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,18 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -52,8 +46,6 @@ fun AppLauncher(
     val pagerState = rememberPagerState(pageCount = { pages.size })
     val backgroundPageColor = 0xF57777
     val backgroundTransparency = 0.95f
-    val coroutineScope = rememberCoroutineScope()
-    val thresholdPx = with(LocalDensity.current) { 25.dp.toPx() }
 
     Box(
         Modifier
@@ -73,7 +65,7 @@ fun AppLauncher(
                         indication = null, // This removes the ripple effect
                         role = Role.Button
                     ) {
-                        if (showAllApps) onToggleAllApps()
+                        onToggleAllApps()
                         showButton = true
                     }
             ) { page ->
